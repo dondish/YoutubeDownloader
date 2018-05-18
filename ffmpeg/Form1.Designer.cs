3fe8d707482536d00eb91cde723ca028cbec05d2
Oded Shapira <odedshapira1@gmail.com>
2018-05-18 20:48:22 +0300

    Finished the downloader and switched to BackgroundWorker

--- ffmpeg/Form1.Designer.cs
+++ ffmpeg/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace ffmpeg
+﻿namespace YoutubeDownloader
 {
     partial class Form1
     {
@@ -29,11 +29,268 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.URLLabel = new System.Windows.Forms.Label();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.folderDiag = new System.Windows.Forms.FolderBrowserDialog();
+            this.DownloadProg = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.download = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.downloader = new System.ComponentModel.BackgroundWorker();
+            this.Cancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // URLLabel
+            // 
+            this.URLLabel.AutoSize = true;
+            this.URLLabel.Location = new System.Drawing.Point(13, 13);
+            this.URLLabel.Name = "URLLabel";
+            this.URLLabel.Size = new System.Drawing.Size(101, 13);
+            this.URLLabel.TabIndex = 0;
+            this.URLLabel.Text = "Please enter the url:";
+            this.URLLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(120, 10);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(151, 20);
+            this.URLTextBox.TabIndex = 1;
+            this.URLTextBox.TextChanged += new System.EventHandler(this.URLTextBox_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // folderDiag
+            // 
+            this.folderDiag.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // DownloadProg
+            // 
+            this.DownloadProg.Location = new System.Drawing.Point(16, 391);
+            this.DownloadProg.Name = "DownloadProg";
+            this.DownloadProg.Size = new System.Drawing.Size(255, 23);
+            this.DownloadProg.TabIndex = 2;
+            this.DownloadProg.Tag = "";
+            this.DownloadProg.Visible = false;
+            this.DownloadProg.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please select the folder to download to:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 22);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Choose...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // download
+            // 
+            this.download.Location = new System.Drawing.Point(71, 362);
+            this.download.Name = "download";
+            this.download.Size = new System.Drawing.Size(75, 23);
+            this.download.TabIndex = 5;
+            this.download.Text = "Download";
+            this.download.UseVisualStyleBackColor = true;
+            this.download.Click += new System.EventHandler(this.download_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 115);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "MP3";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "File Encoding:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(71, 115);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "MP4";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(125, 115);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(50, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "WAV";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(182, 115);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(49, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "OGG";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(238, 115);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(59, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "WEBM";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Download finished!";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 155);
+            this.label4.MaximumSize = new System.Drawing.Size(275, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(261, 26);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "It will try and download the encoding you selected, if it isn\'t available it will" +
+    " download in another encoding";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.UseMnemonic = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(54, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 14;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Visible = false;
+            // 
+            // downloader
+            // 
+            this.downloader.WorkerReportsProgress = true;
+            this.downloader.WorkerSupportsCancellation = true;
+            this.downloader.DoWork += new System.ComponentModel.DoWorkEventHandler(this.downloader_DoWork);
+            this.downloader.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.downloader_ProgressChanged);
+            this.downloader.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.downloader_RunWorkerCompleted);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(156, 362);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 15;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Visible = false;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(296, 436);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.download);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DownloadProg);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.URLLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youtube Downloader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label URLLabel;
+        private System.Windows.Forms.TextBox URLTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.FolderBrowserDialog folderDiag;
+        private System.Windows.Forms.ProgressBar DownloadProg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button download;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker downloader;
+        private System.Windows.Forms.Button Cancel;
     }
 }
 
--- ffmpeg/Form1.cs
+++ ffmpeg/Form1.cs
@@ -2,19 +2,173 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ffmpeg
+namespace YoutubeDownloader
 {
     public partial class Form1 : Form
     {
+        private Process downloadproc = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void URLTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                new Uri(URLTextBox.Text);
+                errorProvider1.SetError(URLTextBox, null);
+            }
+            catch (UriFormatException er)
+            {
+                errorProvider1.SetError(URLTextBox, "This Url is not valid!");
+            }
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderDiag.ShowDialog() == DialogResult.OK)
+            {
+                string path = folderDiag.SelectedPath;
+                Console.WriteLine(path);
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void download_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            string path = folderDiag.SelectedPath;
+            if (path == "")
+            {
+                errorProvider1.SetError(button1, "Path Not Defined!");
+                return;
+            }
+            errorProvider1.SetError(button1, null);
+            Console.WriteLine(errorProvider1.GetError(URLTextBox));
+            if (errorProvider1.GetError(URLTextBox) != "")
+            {
+                errorProvider1.SetError(URLTextBox, "This Url is not valid!");
+                return;
+            }
+            if (URLTextBox.Text == "")
+            {
+                errorProvider1.SetError(URLTextBox, "URL is required!");
+                return;
+            }
+            errorProvider1.SetError(URLTextBox, null);
+            download.Enabled = false;
+            RadioButton b = Controls.OfType<RadioButton>()
+                .FirstOrDefault(r => r.Checked);
+            if (b == null)
+            {
+                errorProvider1.SetError(download, "No encoding was selected!");
+                return;
+            }
+            errorProvider1.SetError(download, null);
+            string extension = b.Text;
+
+            Console.WriteLine(path + "\\" + @"%(title)s-%(uploader)s-%(id)s" + "." + extension);
+            DownloadProg.Visible = true;
+            label5.Visible = true;
+            Cancel.Visible = true;
+            downloader.RunWorkerAsync(new string[]{extension, path + "\\" + @"%(title)s-%(uploader)s-%(id)s.%(ext)s", URLTextBox.Text});
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void downloader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string[] data = (string[]) e.Argument;
+            string encoding = data[0];
+            string dest = data[1];
+            string videourl = data[2];
+            string arg = String.Format("-f best[ext={0}]/(bestvideo+bestaudio)/best -o \"{1}.%(ext)s\" {2}", encoding, dest, videourl);
+            ProcessStartInfo startInfo = new ProcessStartInfo("youtube-dl.exe", arg);
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = true;
+            startInfo.RedirectStandardOutput = true;
+            downloadproc = new Process { StartInfo = startInfo };
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            downloadproc.Start();
+
+            while (!downloadproc.HasExited||!downloadproc.StandardOutput.EndOfStream)
+            {
+                string line = downloadproc.StandardOutput.ReadLine();
+                if (line == null || !line.Contains("%"))
+                    continue;
+                string dat = line.Substring(11);
+                worker.ReportProgress((int)Double.Parse(dat.Substring(0, dat.LastIndexOf("%"))), dat);
+                Thread.Sleep(1);
+            }
+        }
+
+        private void downloader_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DownloadProg.Value = e.ProgressPercentage;
+            label5.Text = (string) e.UserState;
+        }
+
+        private void downloader_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            label5.Visible = false;
+            DownloadProg.Visible = false;
+            DownloadProg.Value = 0;
+            label3.Visible = true;
+            download.Enabled = true;
+            Cancel.Visible = false;
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            downloadproc.Kill();
         }
     }
 }
